021b453af5dd09739cc715b0584a4fe7c844b0ab
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-08-17 20:21:05 +0300

    setup axios and tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,14 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/restaurant.tsx
+++ src/pages/restaurant.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const Restaurant = () => {
+  const query = useQuery({
+    queryKey: ['todos'], queryFn: async () => {
+      const res = await axios.get("http://localhost:3000/restaurant")
+      return res.data
+    }
+  })
+
   return (
-    <div>restaurant</div>
+    <div className=' flex items-start justify-center'>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 
