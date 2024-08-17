c020adc1960ad13d0fb205ae3e1e6285e53a714c
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-08-17 20:20:08 +0300

    using navlink insted of Link

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,30 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Restaurant from './pages/restaurant'
 import Sidebar from './components/Sidebar'
+import Category from './pages/category'
+import Item from './pages/item'
+import CustomerReview from './pages/CustomerReview'
+import Rating from './pages/Rating'
+import Questions from './pages/Questions'
+import Theme from './pages/Theme'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex '>
+    <div className='flex h-screen'>
       <Sidebar />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/restaurant' element={<Restaurant />} />
-
-
+        <Route path='/category' element={<Category />}></Route>
+        <Route path='/item' element={<Item />}></Route>
+        <Route path='/customerReview' element={<CustomerReview />}></Route>
+        <Route path='/rating' element={<Rating />}></Route>
+        <Route path='/questions' element={<Questions />}></Route>
+        <Route path='/theme' element={<Theme />}></Route>
       </Routes>
     </div>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import { AlignVerticalJustifyEndIcon, ChartBarStacked, ChefHat, FileQuestion, LayoutDashboard, MessageCircleCode, ShoppingBasket, Star, SunMoon } from 'lucide-react'
+import { ChartBarStacked, ChefHat, FileQuestion, MessageCircleCode, ShieldBan, ShoppingBasket, Star, SunMoon } from 'lucide-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
     return (
@@ -13,60 +13,88 @@ const Sidebar = () => {
                 </svg>
             </button>
 
-            <aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-52 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50">
+            <aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-72 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100">
                     <ul className="space-y-2 font-medium">
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                                <LayoutDashboard />
-                                <span className="ms-3">Dashboard</span>
+                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group border border-b-4 shadow">
+                            <ShieldBan />
+                                <span className="ms-3">Admin panal</span>
                             </a>
                         </li>
                         <li>
-                            <Link to={'/restaurant'} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
+                            <NavLink to={'/restaurant'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
                                 <ChefHat />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Restaurant</span>
-                            </Link>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
+                            <NavLink to={'/category'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
                                 <ChartBarStacked />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Category</span>
-                            </a>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
+                            <NavLink to={'/items'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
                                 <ShoppingBasket />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Items</span>
-                            </a>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
-                            <MessageCircleCode />
+                            <NavLink to={'/customerReview'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
+                                <MessageCircleCode />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Customer Review</span>
-                            </a>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
-                            <Star />
+                            <NavLink to={'/rating'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
+                                <Star />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Rating</span>
-                            </a>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
-                            <FileQuestion />
+                            <NavLink to={'/questions'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
+                                <FileQuestion />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Questions</span>
-                            </a>
+                            </NavLink>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
-                            <SunMoon />
+                            <NavLink to={'/theme'} className={(navData) =>
+                                navData.isActive
+                                    ? "flex items-center p-2 text-white rounded-lg  bg-gray-500 group"
+                                    : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group "
+                            }>
+                                <SunMoon />
                                 <span className="text-start flex-1 ms-3 whitespace-nowrap">Theme</span>
-                            </a>
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
-            </aside>
+            </aside >
         </>
     )
 }
