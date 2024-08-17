c023a81192ed6d0bb630806bda679546479836a9
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-08-17 18:34:11 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Restaurant from './pages/restaurant'
+import Sidebar from './components/Sidebar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex '>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/res' element={<Restaurant />} />
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
