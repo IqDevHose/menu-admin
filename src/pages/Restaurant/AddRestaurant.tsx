c39a54cae3cb92a6313fd9f0658349c506b8becc
Yaser Asaad <yaser.asaad@iqdevhouse.com>
2024-08-20 15:02:48 +0300

    still

--- src/pages/Restaurant/AddRestaurant.tsx
+++ src/pages/Restaurant/AddRestaurant.tsx
@@ -1,12 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import CreatableSelect from "react-select/creatable";
 type restaurantType = {
   name: string | null;
+  icon: string | null;
+  categories: string[] | null;
   description: string | null;
   accessCode: string | null;
+  primary: string | null;
+  secondary: string | null;
+  bg: string | null;
   image: string | null;
 };
 function AddRestaurant() {
@@ -14,8 +19,18 @@ function AddRestaurant() {
   const [description, setDescription] = useState<string | null>("");
   const [accessCode, setAccessCode] = useState<string | null>("");
   const [uploadImage, setUploadImage] = useState<string | null>();
+  const [primary, setPrimary] = useState<string | null>("");
+  const [secondary, setSecondary] = useState<string | null>("");
+  const [bg, setBg] = useState<string | null>("");
   const navigate = useNavigate();
 
+  const { data: categories } = useQuery({
+    queryKey: ["category"],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:3000/category");
+      return response.data;
+    },
+  });
   const mutation = useMutation({
     mutationFn: (newRest: restaurantType) => {
       return axios.post(`http://localhost:3000/restaurant`, newRest);
@@ -25,14 +40,24 @@ function AddRestaurant() {
     },
   });
   const handleSubmit = () => {
-    mutation.mutate({ name, description, accessCode, image: null });
+    mutation.mutate({
+      name,
+      categories,
+      icon: null,
+      description,
+      accessCode,
+      image: null,
+      primary,
+      secondary,
+      bg,
+    });
   };
 
   return (
     <div className="w-full mx-auto p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-6">Add Restaurant</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form>
         {/* Restaurant Name */}
         <div className="mb-4">
           <label
@@ -49,6 +74,127 @@ function AddRestaurant() {
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             placeholder="Enter restaurant name"
           />
+        </div>
+
+        {/* Category Select */}
+        <div className="mb-4">
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Category Name
+          </label>
+          <CreatableSelect
+            isMulti
+            isClearable
+            options={categories?.items?.map((category: any) => ({
+              value: category.name,
+              label: category.name,
+            }))}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          ></CreatableSelect>
+        </div>
+
+        {/* Category Name */}
+        {/* <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Category Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={category || ""}
+            onChange={(e) => setCategory(e.target.value)}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Enter restaurant name"
+          />
+        </div> */}
+
+        {/* Primary Color */}
+        <div className="mb-4">
+          <label
+            htmlFor="primary"
+            className="block mb-1 text-sm font-medium text-gray-700"
+          >
+            Primary Color
+          </label>
+          <div className="flex items-center gap-2 rounded-md border w-fit p-2 justify-center">
+            <input
+              type="color"
+              id="primary"
+              value={primary || ""}
+              onChange={(e) => setPrimary(e.target.value)}
+              className={` block  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter primary color"
+            />
+            <input
+              type="text"
+              id="primary"
+              value={primary || ""}
+              onChange={(e) => setPrimary(e.target.value)}
+              className={` block border-none  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter primary color"
+            />
+          </div>
+        </div>
+
+        {/* Secondary Color */}
+        <div className="mb-4">
+          <label
+            htmlFor="secondary"
+            className="block mb-1 text-sm font-medium text-gray-700"
+          >
+            Secondary Color
+          </label>
+          <div className="flex items-center gap-2 rounded-md border w-fit p-2 justify-center">
+            <input
+              type="color"
+              id="secondary"
+              value={secondary || ""}
+              onChange={(e) => setSecondary(e.target.value)}
+              className={` block  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter secondary color"
+            />
+            <input
+              type="text"
+              id="secondary"
+              value={secondary || ""}
+              onChange={(e) => setSecondary(e.target.value)}
+              className={` block border-none  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter secondary color"
+            />
+          </div>
+        </div>
+
+        {/* Background Color */}
+        <div className="mb-4">
+          <label
+            htmlFor="bg"
+            className="block mb-1 text-sm font-medium text-gray-700"
+          >
+            Background Color
+          </label>
+          <div className="flex items-center gap-2 rounded-md border w-fit p-2 justify-center">
+            <input
+              type="color"
+              id="bg"
+              value={bg || ""}
+              onChange={(e) => setBg(e.target.value)}
+              className={` block border-none  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter background color"
+            />
+            <input
+              type="text"
+              id="bg"
+              value={bg || ""}
+              onChange={(e) => setBg(e.target.value)}
+              className={` block border-none  h-10 bg-transparent rounded-md sm:text-sm`}
+              placeholder="Enter background color"
+            />
+          </div>
         </div>
 
         {/* Description */}
@@ -109,6 +255,7 @@ function AddRestaurant() {
         <div className="flex justify-end">
           <button
             type="submit"
+            onSubmit={handleSubmit}
             className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Save Changes
