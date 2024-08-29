4865c9e63d4912c668fa2a1cb775fb04de7863d8
Yaser Asaad <yaser.asaad@iqdevhouse.com>
2024-08-29 11:29:36 +0300

    logout button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,14 +4,13 @@ import {
   FileQuestion,
   LayoutDashboard,
   MessageCircleCode,
-  ShieldBan,
   ShoppingBasket,
   Star,
   SunMoon,
 } from "lucide-react";
-import React from "react";
-import { NavLink } from "react-router-dom";
 
+import { NavLink } from "react-router-dom";
+import Logout from "@/pages/Login/Logout";
 const Sidebar = () => {
   return (
     <>
@@ -20,127 +19,132 @@ const Sidebar = () => {
         className="w-80 h-screen "
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-indigo-100">
-          <ul className="space-y-2 font-medium">
-            <li className="mb-3">
-              <NavLink
-                to={"/"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg bg-indigo-800 "
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200 shadow"
-                }
-              >
-                <LayoutDashboard />
-                <span className="ms-3">Admin panal</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/restaurants"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <ChefHat />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Restaurant
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/categories"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <ChartBarStacked />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Category
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/items"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <ShoppingBasket />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Items
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/customerReviews"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <MessageCircleCode />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Customer Review
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/ratings"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <Star />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Rating
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/questions"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <FileQuestion />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Questions
-                </span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/themes"}
-                className={(navData) =>
-                  navData.isActive
-                    ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
-                    : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
-                }
-              >
-                <SunMoon />
-                <span className="text-start flex-1 ms-3 whitespace-nowrap">
-                  Theme
-                </span>
-              </NavLink>
-            </li>
-          </ul>
+        <div className="h-full flex flex-col justify-between px-3 py-4 overflow-y-auto bg-indigo-100">
+          <div className="flex flex-col ">
+            <ul className="space-y-2 font-medium">
+              <li className="mb-3">
+                <NavLink
+                  to={"/"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg bg-indigo-800 "
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200 shadow"
+                  }
+                >
+                  <LayoutDashboard />
+                  <span className="ms-3">Admin panal</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/restaurants"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <ChefHat />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Restaurant
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/categories"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <ChartBarStacked />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Category
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/items"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <ShoppingBasket />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Items
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/customerReviews"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <MessageCircleCode />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Customer Review
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/ratings"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <Star />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Rating
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/questions"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <FileQuestion />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Questions
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/themes"}
+                  className={(navData) =>
+                    navData.isActive
+                      ? "flex items-center p-2 text-white rounded-lg  bg-indigo-500 group"
+                      : "flex items-center p-2 text-indigo-900 rounded-lg hover:bg-indigo-200  group "
+                  }
+                >
+                  <SunMoon />
+                  <span className="text-start flex-1 ms-3 whitespace-nowrap">
+                    Theme
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <Logout />
+          </div>
         </div>
       </aside>
     </>
