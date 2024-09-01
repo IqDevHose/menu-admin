deac105ea2598987322b1513ebef0c40d75ac332
Yaser Asaad <yaser.asaad@iqdevhouse.com>
2024-09-01 14:56:08 +0300

    upload image commented

--- src/pages/Category/AddCategory.tsx
+++ src/pages/Category/AddCategory.tsx
@@ -118,7 +118,7 @@ function AddCategory() {
         </div>
 
         {/* Upload Image */}
-        <div className="mb-4">
+        {/* <div className="mb-4">
           <label
             htmlFor="upload-image"
             className="block text-sm font-medium text-gray-700"
@@ -136,7 +136,7 @@ function AddCategory() {
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             placeholder="Upload an image"
           />
-        </div>
+        </div> */}
 
         {/* Submit Button */}
         <div className="flex justify-end">
