4ff8ebcb9e15425b74f532e88a8795056f6e6e9e
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-09-09 15:21:23 +0300

    add date to the customer review record

--- src/pages/CustomerReview/CustomerReview.tsx
+++ src/pages/CustomerReview/CustomerReview.tsx
@@ -372,17 +372,17 @@ const CustomerReview = () => {
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
-                <th scope="col" className="px-6 py-3 w-4">
+                <th scope="col" className="px-6 py-3 ">
                   <input
                     type="checkbox"
                     checked={selectedItems.length === filteredData?.length}
                     onChange={handleSelectAll}
                   />
                 </th>
-                <th scope="col" className="px-6 py-3 w-4">
-                  #
+                <th scope="col" className="px-6 py-3 ">
+                  Date
                 </th>
-                <th scope="col" className="px-6 py-3 w-4">
+                <th scope="col" className="px-6 py-3 ">
                   Name
                 </th>
                 <th scope="col" className="px-6 py-3">
@@ -400,6 +400,7 @@ const CustomerReview = () => {
                 <th scope="col" className="px-6 py-3">
                   Birthday
                 </th>
+                
                 <th scope="col" className="px-6 py-3"></th>
               </tr>
             </thead>
@@ -414,7 +415,7 @@ const CustomerReview = () => {
                     />
                   </td>
                   <td className="px-6 py-4">
-                    {(currentPage - 1) * itemsPerPage + index + 1}
+                    {new Date(item.createdAt).toLocaleDateString()} - {new Date(item.createdAt).toLocaleTimeString()}
                   </td>
                   <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                     {highlightText(item.name || "", searchQuery)}
